006b5050ba8c84aa2dcbeebc9879c63a00f2eb30
mikolaj <mbuczek111@gmail.com>
2020-03-22 21:05:31 +0100

    my improved homework.

--- AverageCalculator.java
+++ AverageCalculator.java
@@ -1,15 +1,12 @@
 public class AverageCalculator {
     public static void main(String[] args) {
         double dividend =0;
-        double [] myDoubles = new double[args.length];
 
         int divisor=args.length;
         for(int i=0;i<args.length;i++)
         {
-            myDoubles[i] = Double.parseDouble(args[i]);
-            dividend+=myDoubles[i];
+            dividend+= Double.parseDouble(args[i]);
         }
-
         System.out.println(dividend/divisor);
     }
 }
